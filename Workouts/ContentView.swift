4805699cf8fe8e0a38a70339c8f5be1391f2e505
Joseph DeWeese <rangerboy901@icloud.com>
2022-09-15 19:16:44 -0500

    Initial Commit

---
+++ Workouts/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Workouts
+//
+//  Created by Joseph Wil;liam DeWeese on 9/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Workouts/WorkoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutsApp.swift
+//  Workouts
+//
+//  Created by Joseph Wil;liam DeWeese on 9/15/22.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
